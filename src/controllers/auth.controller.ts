28b7629d75a8638f87a92cc971678f786807e709
Taiwo kazeem <kazeem@kobo360.com>
2022-01-03 15:22:22 +0100

    update: enforce no duplicate email and phoneNumber

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getAHospital, registerAHospital } from '../services/auth.service';
-import { hospital } from '../interface/auth.interface';
+import { hospital, hospitalOut } from '../interface/auth.interface';
 import { checkValidity } from '../helper';
 import { hashPassword } from '../helpers/auth.helper';
 import { hospitalRegRule } from '../interface/validators';
@@ -20,12 +20,10 @@ export const registerHospital = async (req: Request, res: Response) => {
         // hash user password
         data.password = await hashPassword(data.password);
 
-        let result = await registerAHospital(req.body);
-        result = result.get({plain: true}); // convert to js object
-        delete result.password 
+        let result: hospitalOut = await registerAHospital(req.body);
         return res.status(201).json({ success: true, data: result });
-    } catch (error) {
-        return res.status(500).json({ success: false, message: error })
+    } catch (error: any) {
+        return res.status(412).json({ success: false, message: error.message })
     }
 
 }
@@ -42,6 +40,6 @@ export const getHospitalDetail = async (req: Request, res: Response) => {
         result === null ? result = {} : result;
         return res.status(200).json({ success: true, data: result })
     } catch (error) {
-        return res.status(500).json({ success: false, message: error })
+        return res.status(500).json({ success: false, message: 'Error occurred when fetching data!' })
     }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,21 +1,37 @@
 import db from '../database/models';
-import { hospital } from '../interface/auth.interface';
+import { hospitalIn } from '../interface/auth.interface';
+const { Op } = require("sequelize");
 
 require('dotenv').config();
 
-export const registerAHospital = async (data: hospital) => {
+export const registerAHospital = async (data: hospitalIn) => {
+
+    // check if email or phone number already exists
+    const hospitalExist = await db.hospital.findOne({
+        where: {
+            [Op.or]: [
+                { email: data.email },
+                { phoneNumber: data.phoneNumber }
+            ]
+        }
+    })
+
+    if (hospitalExist) {
+        throw new Error("An hospital with email or phonenumber already exists");
+    }
     try {
         const newHospital = await db.hospital.create(data);
-        return newHospital;
+        let result = newHospital.get({ plain: true }); // convert to js object
+        delete result.password
+        return result;
     } catch (error) {
-        console.log(error);
         throw new Error('There was an error registering hospital')
     }
 }
 
-export const getAHospital = async(regNo: string) => {
+export const getAHospital = async (regNo: string) => {
     try {
-        const details = await db.hospital.findOne({ where: {hospital_reg:regNo } })
+        const details = await db.hospital.findOne({ where: { hospital_reg: regNo } })
         return details
     } catch (error) {
         console.log(error);
