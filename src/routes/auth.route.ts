3466190e1dfd80acdce80a80fcfb98830d7e1990
unknown <seyi@example.com>
2022-01-06 14:09:20 -1100

    commit

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,28 +1,18 @@
 import express from 'express';
-<<<<<<< HEAD
-import { getHospitalDetail, loginHospital, registerHospital, registerPatient, loginPatient } from '../controllers/auth.controller';
-=======
 import { getHospitalDetail, loginHospital, registerHospital, updateHospital, registerPatient } from '../controllers/auth.controller';
 import { verifyToken } from '../helpers/auth.helper';
->>>>>>> e43603494d3ec183db769bd5c795c99f31f45e71
 
 const authRouter = express.Router();
 
 
 authRouter.post('/register', registerHospital)
 authRouter.post('/login', loginHospital)
-<<<<<<< HEAD
-authRouter.get('/:regNo', getHospitalDetail)
-authRouter.post('/patient/register', registerPatient)
-authRouter.post('/patient/login', loginPatient)
-=======
 authRouter.post('/patient/register', registerPatient)
 authRouter.get('/:regNo', verifyToken,getHospitalDetail)
 
 authRouter.get('/', verifyToken,getHospitalDetail)
 
 authRouter.put('/', verifyToken, updateHospital)
->>>>>>> e43603494d3ec183db769bd5c795c99f31f45e71
 
 
 export default authRouter;
