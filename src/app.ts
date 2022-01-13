a3840086f95085ea9eef13a89ba0828800dcec1b
Taiwo kazeem <kazeem@kobo360.com>
2022-01-13 11:29:00 +0100

    fix: remove CORS issue

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express, {Application, Request, Response} from 'express';
 import authRouter from './routes/auth.route';
+import Cors from 'cors';
+
 const app: Application = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(Cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 // other app config stays here
 app.use('/hospital',authRouter);
@@ -12,6 +21,7 @@ app.use('/hospital',authRouter);
 app.get('/', (req, res)=>{
     res.status(200).send('Server up and runing')
 })
+
 
 
 
